eed4b6b238df750bde612c7fd991c8e91fe1583c
Maristela Rocha <maristela.rocha.silva@gmail.com>
2025-10-31 19:11:08 -0300

    Initiate component Tema

---
+++ src/components/tema/cardtema/CardTema.tsx
@@ -0,0 +1,10 @@
+function CardTema() {
+  return (
+    <div className="border flex flex-col rounded-2x1 overflow-hidden justify-between">
+      <header className="py-2 px-6 bg-indigo-800 text-white font-bold text-2x1">
+        Tema
+      </header>
+    </div>
+  );
+}
+export default CardTema;
